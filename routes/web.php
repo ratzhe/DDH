0b5ee96110e65e516a1792461ee6b14127b55774
ratzhe <81121205+ratzhe@users.noreply.github.com>
2024-04-21 23:43:20 -0300

    Atualização Código

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CadastroController;
-use App\Http\Controllers\Educador;
-use App\Http\Controllers\EducadorController;
-use App\Http\Controllers\MedicoController;
-use App\Http\Controllers\NovoeducadorController;
-use App\Http\Controllers\NovomedicoController;
-use App\Http\Controllers\NovonutricionistaController;
 use App\Http\Controllers\NovousuarioController;
-use App\Http\Controllers\NutricionistaController;
 use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\ProfissionalController;
-use App\Http\Middleware\AuthenticateCustom;
-use App\Models\Medico;
-use App\Models\Nutricionista;
-use App\Http\Middleware\AuthenticateMiddleware;
+
 
 //Login
 Route::get('/site/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
@@ -35,48 +25,6 @@ Route::post('/site/trocarsenha', [LoginController::class, 'trocarsenha'])->middl
 // Cadastro de Usuário Administrador
 Route::get('/site/cadastro', [CadastroController::class, 'cadastrar'])->name('site.cadastro');
 Route::post('/site/cadastro', [CadastroController::class, 'salvar'])->name('site.cadastro');
-
-
-    // Médico
-Route::prefix('/site/medico')->group(function () {
-    Route::get('/', [MedicoController::class, 'index'])->name('site.medico.index');
-    Route::get('/index', [MedicoController::class, 'index'])->name('site.medico.index');
-    Route::post('/index', [MedicoController::class, 'index'])->name('site.medico.index');
-    Route::get('/listar', [MedicoController::class, 'listar'])->name('site.medico.listar');
-    Route::post('/listar', [MedicoController::class, 'listar'])->name('site.medico.listar');
-    Route::get('/adicionar', [MedicoController::class, 'adicionar'])->name('site.medico.adicionar');
-    Route::post('/adicionar', [MedicoController::class, 'adicionar'])->name('site.medico.adicionar');
-    Route::get('/editar/{id}', [MedicoController::class, 'editar'])->name('site.medico.editar');
-    Route::get('/excluir/{id}', [MedicoController::class, 'excluir'])->name('site.medico.excluir');
-    });
-
-
-    // Nutricionista
-Route::prefix('/site/nutricionista')->group(function () {
-    Route::get('/', [NutricionistaController::class, 'index'])->name('site.nutricionista.index');
-    Route::get('/index', [NutricionistaController::class, 'index'])->name('site.nutricionista.index');
-    Route::post('/index', [NutricionistaController::class, 'index'])->name('site.nutricionista.index');
-    Route::get('/listar', [NutricionistaController::class, 'listar'])->name('site.nutricionista.listar');
-    Route::post('/listar', [NutricionistaController::class, 'listar'])->name('site.nutricionista.listar');
-    Route::get('/adicionar', [NutricionistaController::class, 'adicionar'])->name('site.nutricionista.adicionar');
-    Route::post('/adicionar', [NutricionistaController::class, 'adicionar'])->name('site.nutricionista.adicionar');
-    Route::get('/editar/{id}', [NutricionistaController::class, 'editar'])->name('site.nutricionista.editar');
-    Route::get('/excluir/{id}', [NutricionistaController::class, 'excluir'])->name('site.nutricionista.excluir');
-
-});
-
-// Educador Físico
-Route::prefix('/site/educador')->group(function () {
-    Route::get('/', [EducadorController::class, 'index'])->name('site.educador.index');
-    Route::get('/index', [EducadorController::class, 'index'])->name('site.educador.index');
-    Route::post('/index', [EducadorController::class, 'index'])->name('site.educador.index');
-    Route::get('/listar', [EducadorController::class, 'listar'])->name('site.educador.listar');
-    Route::post('/listar', [EducadorController::class, 'listar'])->name('site.educador.listar');
-    Route::get('/adicionar', [EducadorController::class, 'adicionar'])->name('site.educador.adicionar');
-    Route::post('/adicionar', [EducadorController::class, 'adicionar'])->name('site.educador.adicionar');
-    Route::get('/editar/{id}', [EducadorController::class, 'editar'])->name('site.educador.editar');
-    Route::get('/excluir/{id}', [EducadorController::class, 'excluir'])->name('site.educador.excluir');
-});
 
 // Perfil 
 Route::prefix('/site/perfil')->group(function () {
